8948bcfb708775a106b2d4c73c651a77f3446a3f
玄月 <396266318@qq.com>
2018-03-29 10:33:41 +0800

    new complete

--- news/new/app.py
+++ news/new/app.py
@@ -17,17 +17,16 @@ db = SQLAlchemy(app)
 
 class File(db.Model):
     __tablename__ = 'file' # 文件表
+
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
+    title = db.Column(db.String(80), unique=True)
     created_time = db.Column(db.DateTime)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     content = db.Column(db.Text)
-    category = db.relationship('Category')  # 实例化 Category 关系
+    category = db.relationship('Category', uselist=False)  # 实例化 Category 关系
 
     def __init__(self, title, created_time, category_id, content):
         self.title = title
-        # if created_time is None:
-        #     created_time = datetime.utcnow()
         self.created_time = created_time
         self.category = category_id
         self.content = content
@@ -38,6 +37,7 @@ class File(db.Model):
 
 class Category(db.Model):
     __tablename__ = 'category' # 类别表
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
@@ -47,12 +47,24 @@ class Category(db.Model):
     def __repy__(self):
         return '{0}'.format(self.name)
 
+def insert_datas():
+    java = Category('Java')
+    python = Category('Python')
+    file1 = File('Hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
+    file2 = File('Hello Python', datetime.utcnow(), python, 'File Content - Python is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
+
+
 
 @app.route('/')
 def index():
     # 显示文章名称的列表
     # 页面中需要显示 files 目录下所有的json 文件中的 title 信息列表
-    return render_template('index.html', title=File.query.all())
+    return render_template('index.html', files=File.query.all())
 
 
 @app.route('/files/<file_id>')
@@ -60,29 +72,8 @@ def file(file_id):
     # 读取并显示 filename.json 中的文章内容
     # 例如 filename='helloshiyanlou' 的时候显示 helloshiyanlou.json 中的内容
     # 如果 filename 不存在，则显示包含字符串 'shiyanlou 404' 404 错误页面
-
-    # if filename == 'helloshiyanlou':
-    #     content = {
-    #         'title1': new1.title,
-    #         'created_time1': new1.created_time,
-    #         'content1': new1.content,
-    #         'type1': new1.category
-    #     }
-    #     return render_template('file.html', content=content)
-    # elif filename == 'helloworld':
-    #     content = {
-    #         'title2': new2.title,
-    #         'created_time2': new2.created_time,
-    #         'content2': new2.content,
-    #         'type2': new2.category,
-    #     }
-    #     return render_template('file.html', content=content)
-
-    # else:
-    #     abort(404)
     file_items = File.query.get_or_404(file_id)
-
-    return render_template('file.html', items=file_items)
+    return render_template('file.html', file_item=file_items)
 
 
 @app.errorhandler(404)
