47dadf3c1135f38eef497901d7ad293e859bca19
玄月 <396266318@qq.com>
2018-03-30 11:20:43 +0800

    new 1

--- news/new/app.py
+++ news/new/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 from datetime import datetime
 from pymongo import MongoClient
@@ -7,29 +8,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
+app.config.update(dict(
+    SQLALCHEMY_DATABASE_URI='mysql://root@localhost/shiyanlou'
+))
+
 # app.config.update(dict(
 #     SQLALCHEMY_DATABASE_URI='mysql://huxin:l1hg5JdhezWf@192.168.6.68:3306/shiyanlou'))
-app.config.update(dict(
-        SQLALCHEMY_DATABASE_URI='mysql://root@localhost/shiyanlou'))
-
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
 
 db = SQLAlchemy(app)
 mongo = MongoClient('127.0.0.1', 27017).shiyanlou
 
 
 class File(db.Model):
-    __tablename__ = 'files' # 文件表
+    __tablename__ = 'files'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True)
     created_time = db.Column(db.DateTime)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    category = db.relationship('Category', uselist=False)  # 实例化 Category 关系
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship('Category', uselist=False)
     content = db.Column(db.Text)
 
-    def __init__(self, title, created_time, category_id, content):
+    def __init__(self, title, created_time, category, content):
         self.title = title
         self.created_time = created_time
         self.category = category
@@ -41,12 +42,13 @@ class File(db.Model):
             tags = file_item['tags']
             if tag_name not in tags:
                 tags.append(tag_name)
-            mongo.files.update_one({'file_id': self.id}, {'$set': {'tags': tags}})
+            mongo.files.update_one({'file_id': self.id}, {
+                                   '$set': {'tags': tags}})
         else:
             tags = [tag_name]
             mongo.files.insert_one({'file_id': self.id, 'tags': tags})
         return tags
-    
+
     def remove_tag(self, tag_name):
         file_item = mongo.files.find_one({'file_id': self.id})
         if file_item:
@@ -56,10 +58,11 @@ class File(db.Model):
                 new_tags = tags
             except ValueError:
                 return tags
-            mongo.files.update_one({'file.id': self.id}, {'$set': {'tags': new_tags}})
+            mongo.files.update_one({'file_id': self.id}, {
+                                   '$set': {'tags': new_tags}})
             return new_tags
         return []
-    
+
     @property
     def tags(self):
         file_item = mongo.files.find_one({'file_id': self.id})
@@ -70,55 +73,45 @@ class File(db.Model):
             return []
 
 
-    def __repr__(self):
-        return '{0}'.format(self.title)
-
-
-
 class Category(db.Model):
-    __tablename__ = 'categorys' # 类别表
+    __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+    files = db.relationship('File')
 
     def __init__(self, name):
         self.name = name
 
-    def __repy__(self):
-        return '{0}'.format(self.name)
 
 def insert_datas():
     java = Category('Java')
     python = Category('Python')
-    file1 = File('Hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
-    file2 = File('Hello Python', datetime.utcnow(), python, 'File Content - Python is cool!')
+    file1 = File('Hello Java', datetime.utcnow(),
+                 java, 'File Content - Java is cool!')
+    file2 = File('Hello Python', datetime.utcnow(),
+                 python, 'File Content - Python is cool!')
     db.session.add(java)
     db.session.add(python)
     db.session.add(file1)
     db.session.add(file2)
     db.session.commit()
-    file1.add_tag('tach')
+    file1.add_tag('tech')
     file1.add_tag('java')
     file1.add_tag('linux')
     file2.add_tag('tech')
     file2.add_tag('python')
 
 
-
 @app.route('/')
 def index():
-    # 显示文章名称的列表
-    # 页面中需要显示 files 目录下所有的json 文件中的 title 信息列表
     return render_template('index.html', files=File.query.all())
 
 
-@app.route('/files/<file_id>')
+@app.route('/files/<int:file_id>')
 def file(file_id):
-    # 读取并显示 filename.json 中的文章内容
-    # 例如 filename='helloshiyanlou' 的时候显示 helloshiyanlou.json 中的内容
-    # 如果 filename 不存在，则显示包含字符串 'shiyanlou 404' 404 错误页面
-    file_items = File.query.get_or_404(file_id)
-    return render_template('file.html', file_item=file_items)
+    file_item = File.query.get_or_404(file_id)
+    return render_template('file.html', file_item=file_item)
 
 
 @app.errorhandler(404)
@@ -126,5 +119,5 @@ def not_found(error):
     return render_template('404.html'), 404
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+if __name__ == '__main__':
+    app.run()
