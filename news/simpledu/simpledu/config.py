01873be67e8bf03a6df93eaaeeec2949e96c3ad0
玄月 <396266318@qq.com>
2018-05-04 17:14:10 +0800

    simpledu

---
+++ news/simpledu/simpledu/config.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+class BaseConfig(object):
+    """ 配置基类 """
+    SECRET_KEY = 'makesure to set a very secret key'
+
+
+class DevelopmentConfig(BaseConfig):
+    """ 开发环境配置 """
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@192.168.6.68:3306/simpledu?charset=utf8'
+
+
+class ProductionConfig(BaseConfig):
+    """ 生产环境配置 """
+    pass
+
+
+class TestingConfig(BaseConfig):
+    """ 测试环境配置 """
+    pass
+
+
+configs = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
