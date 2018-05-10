e4066c0ea5d1abc092b8d6bc0f148a85775ebd2c
玄月 <396266318@qq.com>
2018-05-10 17:02:09 +0800

    实验26
    实验26

--- news/simpledu/simpledu/app.py
+++ news/simpledu/simpledu/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from simpledu.config import configs
 from simpledu.models import db, Course
+from flask_migrate import Migrate
 
 
 def register_blueprints(app):
@@ -16,6 +17,7 @@ def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
     db.init_app(app)
+    Migrate(app, db)
     register_blueprints(app)
 
     return app
